f8d8fe5f2d80022abbddb0b6e21d8a00b51e825e
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-02 18:48:35 -0400

    246. Running the Ticket Service

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError } from "@oldledger/common";
+
+const app = express();
+app.set("trust proxy", true); // Make express aware that traffic is being proxied to this service by ingress nginx/istio, and configure express to trust that proxy
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    // secure: process.env.NODE_ENV !== 'test' // NOTE: Disable security
+  })
+);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
