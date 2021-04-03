039ac4428a6a3abbb86d9d558f1d042b2375a524
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-02 20:15:16 -0400

    250. Creating the Router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
+import { createTicketRouter } from "./routes/new";
 
 import { errorHandler, NotFoundError } from "@oldledger/common";
 
@@ -14,6 +15,8 @@ app.use(
     // secure: process.env.NODE_ENV !== 'test' // NOTE: Disable security
   })
 );
+
+app.use(createTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
