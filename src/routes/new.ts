05eaa28168bf1b56a07302695f6f5a38187c0ec3
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-05 15:25:19 -0400

    316. Ticket Update Publishing

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -28,8 +28,7 @@ router.post(
 
     await ticket.save();
 
-    new TicketCreatedPublisher(natsWrapper.client).publish({
-      // This service is failing because client is undefined
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
