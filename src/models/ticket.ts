e52fca70dcd9b4fd25b84d1848c18ba0fb65ac67
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-06 04:53:42 -0400

    339. Convenience Document Methods

--- src/models/ticket.ts
+++ src/models/ticket.ts
@@ -1,20 +1,27 @@
 import mongoose from "mongoose";
 
+// Properties used to create a ticket
 interface TicketAttrs {
   title: string;
   price: number;
   userId: string;
 }
 
+// A class that describes the properties of a Ticket Document/
+// database row that can be accessed when using the model in code
 interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
   userId: string;
 }
+
+// A class that provides an interface to the database for reading,
+// creating, querying, updating, deleting records, etc
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
 }
 
+// An interface that defines the structure of a document, default values, validators, etc.
 const ticketSchema = new mongoose.Schema(
   {
     title: {
@@ -31,6 +38,8 @@ const ticketSchema = new mongoose.Schema(
     },
   },
   {
+    // Override the JSON representation of the serialized user model. Useful for
+    // protecting sensitive information before transferring over HTTP for instance
     toJSON: {
       transform(doc, ret) {
         ret.id = ret._id;
@@ -40,10 +49,12 @@ const ticketSchema = new mongoose.Schema(
   }
 );
 
+// The statics object allows us to add a new method to the model
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
 };
 
+// The model provides us access to the collection (database table), in code
 const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
 
 export { Ticket };
