c451867c86b73adb2d2dabc6aada39cac5b64b8f
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-08 08:15:34 -0400

    372. One More Test

--- src/models/__test__/ticket.test.ts
+++ src/models/__test__/ticket.test.ts
@@ -44,3 +44,18 @@ it("implements optimistic concurrency control", async (done) => {
   //     await secondInstance!.save();
   //   }).toThrow(mongoose.Error.VersionError);
 });
+
+it("increments the version number on multiple saves", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+    userId: "123",
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
