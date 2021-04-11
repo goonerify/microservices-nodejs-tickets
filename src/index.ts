db197e6b694091b696cc775c670735c158a61849
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-11 05:07:58 -0400

    422. Initial Setup

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ const start = async () => {
   try {
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
-      process.env.NATS_CLIENT_ID, // Client ID needs to be unique for every instance of the ticketing service
+      process.env.NATS_CLIENT_ID,
       process.env.NATS_URL
     );
 
